448af764b8af1419be4cb16804075fa753e15f58
Onelovetechworld <nexatechx@gmail.com>
2024-11-21 11:33:34 +0100

    New Updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Route, Routes,} from 'react-router-dom'
 import React from 'react'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+
 
 export default function App() {
   return (
-    <div className='text-3xl text-red-500'>app</div>
+    <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/SignIn" element={<SignIn />} />
+      <Route path="/SignUp" element={<SignUp />} />
+      <Route path="/Dashboard" element={<Dashboard />} />
+      <Route path="/Projects" element={<Projects />} />
+     </Routes>
+    
+    </BrowserRouter>
   )
 }
